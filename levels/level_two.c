0c42d1ae9437ddaeb88bba67c9d9ec7930f9ab79
xbuchm01 <230750@vut.cz>
2023-04-23 05:03:55 -0400

    fixed exploit and changed difficutly

--- levels/level_two.c
+++ levels/level_two.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
     unsigned int generated = rand();
@@ -7,25 +7,27 @@ int main(int argc, char *argv[]) {
     if (argv[1]){       
             tries = 1; 
     }
-    unsigned int key, mix;
+    char key[10], mix;
     while(tries != 0) {
-        printf("provide correct key to get to third level. Tries left: %d\n", tries);
+        //printf("provide correct key to get to third level. Tries left: %d\n", tries);
         if (argv[1]){         
-            key = atoi(argv[0]);
+            //key = argv[1];
         } else {
             puts("key:"); // 3039275914
-            scanf("%u", &key);
+            scanf("%s", &key);
         }
-        
+        /*
         mix = generated;
 
         for(int i = 0; i < 0xd; i++) {
             mix = (mix + 0xffe) ^ key;
         }
-
+        */
 
         //printf("mix is %x\n", mix);
-        if( mix == 0xdeadbeef ){
+        //if( mix == 0xdeadbeef ){
+        //printf("%s \n", argv[1]);
+        if (strcmp(argv[1], "1025") == 0) {
             puts("continuing to level three");
             return 0;
         }
