66ea2305220c8f0bc79847e44571753d6f699a2d
xbuchm01 <230750@vut.cz>
2023-05-01 16:18:48 -0400

    Easy password for demonstration

--- levels/level_two.c
+++ levels/level_two.c
@@ -2,30 +2,23 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    unsigned int generated = rand();
     int tries = 3;
     if (argv[1]){       
             tries = 1; 
     }
-    unsigned int key, mix;
+    int key;
     while(tries != 0) {
-        printf("provide correct key to get to third level. Tries left: %d\n", tries);
+        //printf("provide correct key to get to third level. Tries left: %d\n", tries);
         if (argv[1]){         
-            key = atoi(argv[0]);
+            key = atoi(argv[1]);
         } else {
             puts("key:");
             scanf("%u", &key);
         }
-        
-        mix = generated;
-
-        for(int i = 0; i < 0xd; i++) {
-            mix = (mix + 0xffe) ^ key;
-        }
 
 
-        //printf("mix is %x\n", mix);
-        if( mix == 0xdeadbeef ){
+        printf("key is %d\n", key);
+        if( key == 9641){
             puts("continuing to level three");
             return 0;
         }
